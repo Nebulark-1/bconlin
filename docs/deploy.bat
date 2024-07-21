ac722e0b22c8855df4b8e4f4a15d339eda84d399
Ben Conlin <73730588+Nebulark-1@users.noreply.github.com>
2024-07-20 18:17:26 -0600

    Added deploy.bat, included dist

--- docs/deploy.bat
+++ docs/deploy.bat
@@ -3,19 +3,29 @@ REM Exit on errors
 setlocal enabledelayedexpansion
 if errorlevel 1 exit /b 1
 
+REM Navigate to the project directory
+cd /d %~dp0
+
 REM Build the project
 npm run build
 
-REM Navigate into the build output directory
-cd dist
+REM Create a new directory to temporarily hold the build output
+mkdir temp-deploy
+xcopy dist temp-deploy /E /I
 
-REM Initialize a new git repository
-git init
-git add -A
-git commit -m "deploy"
+REM Checkout to gh-pages branch
+git checkout gh-pages
 
-REM Force push to the gh-pages branch
-git push -f git@github.com:<Nebulark-1>/<bconlin>.git master:gh-pages
+REM Copy the build output to the root of the gh-pages branch
+xcopy temp-deploy\* . /E /I /Y
 
-REM Go back to the root of the project
-cd ..
+REM Clean up the temporary directory
+rmdir /S /Q temp-deploy
+
+REM Add, commit, and push the changes
+git add .
+git commit -m "Deploy latest build"
+git push origin gh-pages
+
+REM Go back to the previous branch
+git checkout -
